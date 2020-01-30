d8b185be935a5ffdfb6caedc0748209887a40d28
fadimata tillman <fadi.tillman@gmail.com>
2020-01-30 21:02:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,20 +7,20 @@ else
   deli.each_with_index(1) do |person, i|
     str += " #{i}. #{person}"
   end
-puts str
- end
-end
+#puts str
+ #end
+#end
 
-def take_a_number(deli, person)
-  deli << person
-  puts "welcome, #{person}, you are number #{deli,index()} in line"
-end
+#def take_a_number(deli, person)
+  #deli << person
+  #puts "welcome, #{person}, you are number #{deli,index()} in line"
+#end
 
-def now_serving(deli)
-  if deli.length ==0
-    puts "The line is currently empty."
-  else
-    puts "currently serving #{deli(0)}"
-    deli.shift
-  end
-end
+#def now_serving(deli)
+  #if deli.length ==0
+  #  puts "The line is currently empty."
+  #else
+    #puts "currently serving #{deli(0)}"
+    #deli.shift
+  #end
+#end
