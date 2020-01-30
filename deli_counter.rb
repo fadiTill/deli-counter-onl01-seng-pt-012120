e9f5d7d8c1c4d8d2c1efaec4fe000fa5b4e63f0b
fadimata tillman <fadi.tillman@gmail.com>
2020-01-30 21:01:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,26 @@
 # Write your code here.
+def line(deli)
+  if deli.length == 0
+puts  "The line is currently empty."
+else
+  str = "The line is currently empty."
+  deli.each_with_index(1) do |person, i|
+    str += " #{i}. #{person}"
+  end
+puts str
+ end
+end
+
+def take_a_number(deli, person)
+  deli << person
+  puts "welcome, #{person}, you are number #{deli,index()} in line"
+end
+
+def now_serving(deli)
+  if deli.length ==0
+    puts "The line is currently empty."
+  else
+    puts "currently serving #{deli(0)}"
+    deli.shift
+  end
+end
